b4bc4146c77da2a8b20bfbcf6efcb7c75e9732d5
Randall Wood <297232+rhwood@users.noreply.github.com>
2020-09-28 21:35:32 -0400

    more comments

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# sonarsource-is-a-contrarian
+# Sonarsource is a Contrarian
+
+See [the analysis](https://sonarcloud.io/code?id=rhwood%3Asonarsource.is.a.contrarian&selected=rhwood%3Asonarsource.is.a.contrarian%3Asrc%2Ftest%2Fjava%2Fsonarsource%2Fis%2Fa%2Fcontrarian%2FComparableTest.java) and expand the "code smells" (especially the first four)
--- src/test/java/sonarsource/is/a/contrarian/ComparableTest.java
+++ src/test/java/sonarsource/is/a/contrarian/ComparableTest.java
@@ -16,7 +16,9 @@ class ComparableTest {
         // these two statements have contradictory code smells
         assertThat("bar".compareTo("foo")).isNegative();
         assertThat("bar".compareTo("foo")).isLessThan(0);
-        // these two statements have contradictory code smells
+        // these two statements have code smells, but the third does not, yet
+        // it seems these should be preferable to the last based on the comments
+        // in java:S5838
         assertThat("foo".compareTo("foo")).isZero();
         assertThat("foo".compareTo("foo")).isEqualTo(0);
         assertThat("foo".compareTo("foo")).isEqualByComparingTo(0);
