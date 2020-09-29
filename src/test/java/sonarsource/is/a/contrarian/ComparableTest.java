7e0c4eaaaa458954102d675e505460f0e8c950e1
Randall Wood <297232+rhwood@users.noreply.github.com>
2020-09-28 21:26:26 -0400

    add comments

--- src/test/java/sonarsource/is/a/contrarian/ComparableTest.java
+++ src/test/java/sonarsource/is/a/contrarian/ComparableTest.java
@@ -3,16 +3,23 @@ package sonarsource.is.a.contrarian;
 import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
 
+/**
+ * Simple test that demonstrates some contrarian behaviors in SonarCloud.
+ */
 class ComparableTest {
 
     @Test
     void testCompareTo() {
+        // these two statements have contradictory code smells
         assertThat("foo".compareTo("bar")).isPositive();
         assertThat("foo".compareTo("bar")).isGreaterThan(0);
+        // these two statements have contradictory code smells
         assertThat("bar".compareTo("foo")).isNegative();
         assertThat("bar".compareTo("foo")).isLessThan(0);
+        // these two statements have contradictory code smells
         assertThat("foo".compareTo("foo")).isZero();
         assertThat("foo".compareTo("foo")).isEqualTo(0);
+        assertThat("foo".compareTo("foo")).isEqualByComparingTo(0);
     }
 
 }
